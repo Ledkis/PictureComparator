ffa3fb5f24598cf1b0e6f0c6d25b2f7d98af8af6
Ledkis <emile.quenee@gmail.com>
2015-10-20 15:18:17 +0200

    Add AirHockey Touch OpenGl project

---
+++ app/src/main/java/ledkis/module/picturecomparator/objects/Puck.java
@@ -0,0 +1,50 @@
+/***
+ * Excerpted from "OpenGL ES for Android",
+ * published by The Pragmatic Bookshelf.
+ * Copyrights apply to this code. It may not be used to create training material, 
+ * courses, books, articles, and the like. Contact us if you are in doubt.
+ * We make no guarantees that this code is fit for any purpose. 
+ * Visit http://www.pragmaticprogrammer.com/titles/kbogla for more book information.
+***/
+package ledkis.module.picturecomparator.objects;
+
+import java.util.List;
+
+import ledkis.module.picturecomparator.data.VertexArray;
+import ledkis.module.picturecomparator.objects.ObjectBuilder.DrawCommand;
+import ledkis.module.picturecomparator.objects.ObjectBuilder.GeneratedData;
+import ledkis.module.picturecomparator.programs.ColorShaderProgram;
+import ledkis.module.picturecomparator.util.Geometry.Cylinder;
+import ledkis.module.picturecomparator.util.Geometry.Point;
+
+public class Puck {
+    private static final int POSITION_COMPONENT_COUNT = 3;
+
+    public final float radius, height;
+
+    private final VertexArray vertexArray;
+    private final List<DrawCommand> drawList;
+
+    public Puck(float radius, float height, int numPointsAroundPuck) {
+        GeneratedData generatedData = ObjectBuilder.createPuck(new Cylinder(
+            new Point(0f, 0f, 0f), radius, height), numPointsAroundPuck);
+
+        this.radius = radius;
+        this.height = height;
+
+        vertexArray = new VertexArray(generatedData.vertexData);
+        drawList = generatedData.drawList;
+    }
+
+    public void bindData(ColorShaderProgram colorProgram) {
+        vertexArray.setVertexAttribPointer(0,
+            colorProgram.getPositionAttributeLocation(),
+            POSITION_COMPONENT_COUNT, 0);
+    }
+
+    public void draw() {
+        for (DrawCommand drawCommand : drawList) {
+            drawCommand.draw();
+        }
+    }
+}
